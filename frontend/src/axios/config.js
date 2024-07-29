02ecd1f651d77b057683904e46e7912acde5b5c8
Germano Marcos <germano.mja@gmail.com>
2024-07-29 20:51:58 -0300

    atualização

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const provaFetch = axios.create({
-  baseURL: "http://18.231.106.218:3000/api/",
+  baseURL: "http://54.233.12.207:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
